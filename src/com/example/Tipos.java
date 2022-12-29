5f199569d09a729f1a685179e2bb7c3facc2bdc5
Orbexciii <120743257+Orbexciii@users.noreply.github.com>
2022-12-29 12:33:33 +0100

    Add files via upload
    Ejercicios tipos de datos

---
+++ src/com/example/Tipos.java
@@ -0,0 +1,45 @@
+package com.example;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+       // 1 Datos de tipo númerico
+
+           //1.1 Enteros
+
+            byte c = 6;
+            System.out.println(c);
+            short s = 10;
+            System.out.println(s);
+            int i = 10;
+            System.out.println(i);
+            short p = 10;
+            System.out.println(p);
+            int a = 50;
+            System.out.println(a);
+            long telefono = 965425800;
+            System.out.println(telefono);
+
+            //1.2 decimales
+
+            float f = 10.5f;
+            System.out.println(f);
+            double d = 100.5d;
+            System.out.println(d);
+
+         //2 Datos booleanos (True, false)
+
+            boolean b = true;
+            System.out.println(b);
+
+        //3 Texto
+
+            char x = 'a';
+            System.out.println(a);
+            String texto = "Lorem ipsum...";
+            System.out.println(texto);
+
+
+
+    }
+}
