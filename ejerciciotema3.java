865b0671c48a212f11b773962000eee688421adb
Orbexciii <120743257+Orbexciii@users.noreply.github.com>
2023-01-03 20:30:09 +0100

    Add files via upload

---
+++ ejerciciotema3.java
@@ -0,0 +1,27 @@
+package com.example.estructurascontrol.repeticion;
+
+import java.util.Arrays;
+
+public class ejerciciotema3 {
+    public static void main(String[] args) {
+        String[] nombres = {"Juan", "Carlos", "Pepe"};
+        for (String nombre : nombres) {
+        }
+                System.out.println((Arrays.toString(nombres)));
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
